61078eca333e30b469893f5ae40a5980c43c5200
Aydeniz <88689203+Aydeniztr@users.noreply.github.com>
2022-07-29 16:47:59 +0300

    Update ss.py

--- ss.py
+++ ss.py
@@ -20,9 +20,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-# foreground colors
-
- FNRM = "\x1B[0m"
+FNRM = "\x1B[0m"
  FRED = "\x1B[31m"
  FGRN = "\x1B[32m"
  FYEL = "\x1B[33m"
@@ -30,10 +28,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
  FMAG = "\x1B[35m"
  FCYN = "\x1B[36m"
  FWHT = "\x1B[37m"
- 
-# background colors
- 
- BNRM = "\x1B[0m"
+
+BNRM = "\x1B[0m"
  BRED = "\x1B[41m"
  BGRN = "\x1B[42m"
  BYEL = "\x1B[43m"
@@ -41,10 +37,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
  BMAG = "\x1B[45m"
  BCYN = "\x1B[46m"
  BWHT = "\x1B[47m"
-
-def new_ln():
-
-  print("\n")
 
 def clear_scrn(mode):
 
@@ -70,27 +62,34 @@ def parse_scripts(link):
     print(script_src)
 
   print("\n"+BGRN+"[Done !!!]"+BNRM+"\n")
-  
-  def get_msg(link):
+
+def get_msg(link):
 
   inv = str(link)
+
   print('sending http request to the:\n' + inv)
 
   meta = urlopen(link)
-
+  
   print('\nparsing http-return-message\n')
 	
   u = meta.info()
 	
   print(u)
+  print("\n"+BGRN+"[Account Exist]"+BNRM+"\n")
+
 
 def main(link):
   
+  print(banner)
+  print(license)
   clear_scrn(1)
-  parse_scripts(link)
-  get_msg(link)
+  try:
+    parse_scripts(link)
+    get_msg(link)
+  except:
+    print(BRED+"\n[Account Not Found]\n"+BNRM)
 
-print(banner)
-print(license)
-username = str(input("\n enter a username:"))
+username = str(input("\nenter a username:"))
+
 main("https://www.snapchat.com/add/"+username)
