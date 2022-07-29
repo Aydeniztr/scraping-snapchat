4f4e64c85b7ce08db0c675e5a0aac39a54f64256
Aydeniz <88689203+Aydeniztr@users.noreply.github.com>
2022-07-29 17:09:17 +0300

    Update ss.py

--- ss.py
+++ ss.py
@@ -21,74 +21,74 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 '''
 
 FNRM = "\x1B[0m"
- FRED = "\x1B[31m"
- FGRN = "\x1B[32m"
- FYEL = "\x1B[33m"
- FBLU = "\x1B[34m"
- FMAG = "\x1B[35m"
- FCYN = "\x1B[36m"
- FWHT = "\x1B[37m"
+FRED = "\x1B[31m"
+FGRN = "\x1B[32m"
+FYEL = "\x1B[33m"
+FBLU = "\x1B[34m"
+FMAG = "\x1B[35m"
+FCYN = "\x1B[36m"
+FWHT = "\x1B[37m"
 
 BNRM = "\x1B[0m"
- BRED = "\x1B[41m"
- BGRN = "\x1B[42m"
- BYEL = "\x1B[43m"
- BBLU = "\x1B[44m"
- BMAG = "\x1B[45m"
- BCYN = "\x1B[46m"
- BWHT = "\x1B[47m"
+BRED = "\x1B[41m"
+BGRN = "\x1B[42m"
+BYEL = "\x1B[43m"
+BBLU = "\x1B[44m"
+BMAG = "\x1B[45m"
+BCYN = "\x1B[46m"
+BWHT = "\x1B[47m"
 
 def clear_scrn(mode):
 
-  if mode == 0:
-    print("\x1B[2K")
+	if mode == 0:
+		print("\x1B[2K")
 
-  elif mode == 1:
-    print("\x1B[2J")
+	elif mode == 1:
+		print("\x1B[2J")
 
-  else:
-    print("\x1B[2J")
+	else:
+		print("\x1B[2J")
 
 def parse_scripts(link):
 
-  HTML = urlopen(link)
-  soup = BeautifulSoup (HTML.read(), 'html.parser')
+	HTML = urlopen(link)
+	soup = BeautifulSoup (HTML.read(), 'html.parser')
 
-  print("parsing scripts:\n")
+	print("parsing scripts:\n")
 
-  list_ = [dictionary.get('src') for dictionary in soup.find_all('script') if dictionary.get('src')]
+	list_ = [dictionary.get('src') for dictionary in soup.find_all('script') if dictionary.get('src')]
 
-  for script_src in list_ :
-    print(script_src)
+	for script_src in list_ :
+		print(script_src)
 
-  print("\n"+BGRN+"[Done !!!]"+BNRM+"\n")
+	print("\n"+BGRN+"[Done !!!]"+BNRM+"\n")
 
 def get_msg(link):
 
-  inv = str(link)
+	inv = str(link)
 
-  print('sending http request to the:\n' + inv)
+	print('sending http request to the:\n' + inv)
 
-  meta = urlopen(link)
-  
-  print('\nparsing http-return-message\n')
+	meta = urlopen(link)
 	
-  u = meta.info()
+	print('\nparsing http-return-message\n')
 	
-  print(u)
-  print("\n"+BGRN+"[Account Exist]"+BNRM+"\n")
+	u = meta.info()
+	
+	print(u)
+	print("\n"+BGRN+"[Account Exist]"+BNRM+"\n")
 
 
 def main(link):
-  
-  print(banner)
-  print(license)
-  clear_scrn(1)
-  try:
-    parse_scripts(link)
-    get_msg(link)
-  except:
-    print(BRED+"\n[Account Not Found]\n"+BNRM)
+	
+	print(banner)
+	print(license)
+	clear_scrn(1)
+	try:
+		parse_scripts(link)
+		get_msg(link)
+	except:
+		print(BRED+"\n[Account Not Found]\n"+BNRM)
 
 username = str(input("\nenter a username:"))
 
